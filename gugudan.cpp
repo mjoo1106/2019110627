07edb90cfa6e57cd9631e9bcd63ac6bbd04180e2
JuhwanMaeng <philosophist@khu.ac.kr>
2021-10-08 10:48:29 +0900

    Implementing 9 table

--- gugudan.cpp
+++ gugudan.cpp
@@ -1,6 +1,6 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 int main(void) {
-	for (int i = 1; i <= 8; i++) {
+	for (int i = 1; i <= 9; i++) {
 		printf("%d단\n", i);
 		for (int j = 1; j <= 9; j++)
 			printf("%d x %d = %d\n", i, j, i * j);
