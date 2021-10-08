fd46b314c2fb68e03e592a5de3b38f50ab65365b
김명주 <mjoo1106@khu.ac.kr>
2021-10-08 10:31:18 +0900

    New gugudan

---
+++ gugudan.cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+int main(void) {
+	for (int i = 1; i <= 8; i++) {
+		printf("%d단\n", i);
+		for (int j = 1; j <= 9; j++)
+			printf("%d x %d = %d\n", i, j, i * j);
+	}
+	return 0;
+}
